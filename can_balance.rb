c7db35dbdce0f827306f4ff98d434c71e6b86a13
Arjun Iyer <arjuni142005@gmail.com>
2019-10-07 22:09:42 -0600

    Improviming List challenges

--- can_balance.rb
+++ can_balance.rb
@@ -1,7 +1,19 @@
-def can_balance(str)
-    str.each do |i|
-        
-
+def balanced(list)
+    sum1 = 0
+    total = 0
+    list.each do |n|
+        total = total + n
+    end
+    target_sum = (total/2) 
+    list.each do |i|
+        sum1 = sum1 + i
+        if sum1 == target_sum
+            return true
+        end
+    end
+    return false
 end
 
-puts can_balance([5,7,1,5,7,3,5,3])
+puts balanced([5, 7, 1, 5, 7, 3, 5, 3])#true
+puts balanced([10, 3, 2,4, 1])#true
+puts balanced([6,4,5,2])#false
--- count_code.rb
+++ count_code.rb
@@ -2,10 +2,14 @@ def count_code(str)
     count = 0
     (str.size-2).times do |i|
         slice = str[i..i+3]
-        if slice == "code"
+        sliced1 = slice[0]
+        sliced2 = slice[1]
+        sliced3 = slice[3]
+        if sliced1 + sliced2 + sliced3 == "coe"
             count = count + 1
         end
     end
+    
     return count
 end
 
